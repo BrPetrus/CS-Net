b0f0967359a6fbd155c5c820d68a8952e45c3df7
Bruno Petrus <xpetrus@fi.muni.cz>
2025-02-10 18:32:42 +0100

    feat: add random cropping to dataloader.

--- dataloader/VascuSynthLoader.py
+++ dataloader/VascuSynthLoader.py
@@ -35,7 +35,16 @@ class Data(Dataset):
         # np_stack /= 255.0 
         # np_stack = np_stack.transpose(1, 2, 0)  # HWC
         return np.array(stack)
+    
+    def _random_crop(self, image: NDArray, label: NDArray, crop_size) -> Tuple[NDArray, NDArray]:
+        w, h, d = image.shape
+        z = random.randint(0, w - crop_size[0])
+        y = random.randint(0, h - crop_size[1])
+        x = random.randint(0, d - crop_size[2])
 
+        image = image[z:z + crop_size[0], y:y + crop_size[1], x:x + crop_size[2]]
+        label = label[z:z + crop_size[0], y:y + crop_size[1], x:x + crop_size[2]]
+        return image, label
 
     def _load_dataset(self):
         root_path = Path(self.root_dir)
@@ -72,17 +81,17 @@ class Data(Dataset):
             # gt = gt[:64, :104, :112]
             # img = img[:100, :100, :100]
             # gt = gt[:100, :100, :100]
-            img = img[:64, :64, :64]
-            gt = gt[:64, :64, :64]
+            # img = img[:64, :64, :64]
+            # gt = gt[:64, :64, :64]
 
 
-            # Expand dimensions
-            img = torch.from_numpy(np.ascontiguousarray(img)).unsqueeze(0)
-            gt = torch.from_numpy(np.ascontiguousarray(gt)).unsqueeze(0)
+            # # Expand dimensions
+            # img = torch.from_numpy(np.ascontiguousarray(img)).unsqueeze(0)
+            # gt = torch.from_numpy(np.ascontiguousarray(gt)).unsqueeze(0)
 
-            # Normalize
-            img = img / 255.0
-            gt = gt // 255
+            # # Normalize
+            # img = img / 255.0
+            # gt = gt // 255
 
             images.append(img)
             groundtruth.append(gt)
@@ -99,6 +108,19 @@ class Data(Dataset):
     def __getitem__(self, idx) -> Optional[Tuple[NDArray[np.float32], NDArray[np.int64]]]:
         if idx >= len(self.images):
             return None
-        # return self.images[idx], self.groundtruth[idx]
-        return self.images[idx], self.groundtruth[idx]
+        data, label = self.images[idx], self.groundtruth[idx]
+
+        # Apply the random crop
+        data, label = self._random_crop(data, label, (64, 64, 64))
+
+        # Expand dimensions
+        data = torch.from_numpy(np.ascontiguousarray(data)).unsqueeze(0)
+        label = torch.from_numpy(np.ascontiguousarray(label)).unsqueeze(0)
+
+        # Normalize
+        data /= 255.0
+        label //= 255
+        return data, label
+        
+
 
